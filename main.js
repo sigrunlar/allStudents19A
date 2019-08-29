b28ea38cb1f43905810c62630e866706e6ebde63
Lasse Claes Jacobsen <lacj@windows-idlhl8g.kea.local>
2019-08-29 10:53:38 +0200

    start

---
+++ main.js
@@ -0,0 +1,716 @@
+const students = [
+    {
+        "name": "Liat",
+        "birthday": "1995-10-31",
+        "nationality": "",
+        "studyProgramme": "",
+        "children": [],
+        "skills": [
+            {
+                "name": "JS",
+                "level": 2
+        },
+            {
+                "name": "CSS",
+                "level": 3
+        }
+    ],
+
+        "gender": "",
+        "iAmThishScaredOfJs": 1
+
+},
+    {
+        "name": "Lisa",
+        "birthday": "1995-07-21",
+        "nationality": ["Venezuelan", "German"],
+        "children": [0],
+        "skills": [
+            {
+                "name": "Potatofarming",
+                "level": 1
+        },
+            {
+                "name": "Pipers favourite lady",
+                "level": 10
+        }
+    ],
+        "gender": "Female",
+        "iAmThisScaredOfJS": "level100",
+},
+    {
+
+        "name": "Jonas",
+        "birhday": "19-08-1994",
+        "nationality": "Lithuanian",
+        "studyProgramme": "Multimedia Design",
+        "children": [],
+        "skills": [
+            {
+                "name": "video",
+                "level": 10
+        },
+            {
+                "name": "cycling",
+                "level": 12
+        }
+    ],
+        "gender": "male",
+        "iAmThisScaredOfJS": "what is js?"
+},
+    {
+        "name": "Timi",
+        "birthday": "1999-04-01",
+        "nationality": "Romanian",
+        "studyProgramme": "MMD",
+        "chidlren": [],
+        "skills": [
+            {
+                "name": "Binge",
+                "level": 10
+       },
+            {
+                "name": "Beer",
+                "level": 8
+       },
+            {
+                "name": "Talking",
+                "level": 11
+       }
+    ],
+        "gender": "Female",
+        "iAmThisScaredOfJS": "very",
+},
+    {
+        "name": "Monika",
+        "birthday": "1996-09-29",
+        "nationality": "Slovak",
+        "studyProgramme": "MMD",
+        "children": [],
+        "skills": [
+            {
+                "name": "JS",
+                "level": 3
+        },
+            {
+                "name": "CSS",
+                "level": 1
+        }
+    ],
+        "gender": "female",
+        "iAmThisScaredOfJS": "noneSaid"
+
+},
+    {
+        "name": "Graeme",
+        "birthday": "1979-01-22",
+        "nationality": "Scottish",
+        "studyProgramme": "mmdint",
+        "children": [1],
+        "skills": [
+            {
+                "name": "music",
+                "level": 8
+        },
+            {
+                "name": "dancing",
+                "level": 1
+        }
+    ],
+        "gender": "male",
+        "iAmThisScaredOfJS": "10",
+},
+    {
+        "name": "Gea",
+        "birthday": "1991-03-02",
+        "nationality": "Romanian",
+        "studyProgramme": "MMD",
+        "children": [],
+        "skills": [
+            {
+                "name": "Photography",
+                "level": 10
+        },
+            {
+                "name": "Cats",
+                "level": 10
+        }
+    ],
+        "gender": "Female",
+        "iAmThisScaredOfJS": 7
+},
+    {
+        "name": "RIA",
+        "birthday": "1998-10-21",
+        "nationality": "Croatian",
+        "studyprogramme": "MultimediaDesign",
+        "children": [0],
+        "skills": [
+            {
+                "name": "JS",
+                "level": 1,
+	 },
+
+            {
+                "name": "CSS",
+                "level": 1
+	 }
+ ],
+
+        "gender": "female",
+
+        "iamThisScardedPfJS": 100,
+},
+    {
+        "name": "Liam",
+        "birthday": "1995-10-07",
+        "nationality": "English",
+        "studyProgramme": "MMD",
+        "children": [0],
+        "skills": [{
+                "name": "Drawing",
+                "level": 2
+    }, {
+                "name": "Bartender",
+                "level": 8
+        },
+
+              ],
+        "gender": "Male",
+        "iAMThisScaredOfJS": "kinda yeah a bit"
+},
+    {
+        "name": "Kristaps",
+        "birthday": "1993-03-07",
+        "nationality": "latvian",
+        "studyProgramme": "Multimedia Design",
+        "children": [],
+        "skills": [
+            {
+                "name": "sleeping",
+                "level": 5
+        },
+            {
+                "name": "eating",
+                "level": 6
+        }
+    ],
+        "gender": "male",
+        "iAmThisScaredOfJS": "a bit",
+},
+    {
+        "name": "Lisa",
+        "birthday": "1983-06-28",
+        "nationality": "German",
+        "StudyProgramme": "MMD",
+        "children": [0],
+
+        "skills": [
+
+            {
+                "name": "Sleeping ",
+                "level": 10
+        },
+            {
+                "name": "drinking coffee",
+                "level": 10
+        }],
+        "gender": "female",
+
+
+},
+    {
+        "firstName": "Andre",
+        "surname": "Harris",
+        "birthday": "1989-08-21",
+        "nationality": "British",
+        "studyProgramme": "",
+        "children": [
+            {
+                "firstName": "Zachariah",
+                "age": 3,
+        },
+    ],
+        "skills": [
+            {
+                "name": "Football",
+                "level": 3,
+        },
+            {
+                "name": "talking",
+                "level": 1,
+        }
+    ],
+        "gender": "Male",
+        "IAmThisScaredOfJS": "Very",
+
+},
+    {
+        "name": "Andy",
+        "birthday": "1995/03/11",
+        "nationality": "Romanian",
+        "studyProgram": "MMD",
+        "children": [],
+        "skills": [
+            {
+                "name": "acting",
+                "level": "experienced",
+        },
+            {
+                "name": "cooking",
+                "level": "experienced",
+
+        }
+
+    ],
+        "gender": "male",
+        "iAmThisScaredOfJS": "anxious",
+
+},
+    {
+        "name": "Anna",
+        "birthday": "1993-10-25",
+        "nationality": "ukrainian",
+        "studyProgramme": "MMD",
+        "children": [0],
+        "skills": [
+            {
+                "name": "baking",
+                "level": 10
+        },
+            {
+                "name": "singing",
+                "level": 0
+        }
+
+
+    ],
+        "gender": "Female",
+        "iAmThisScaredOfJS": "wrote: IamsooScaredofhomework"
+},
+    {
+        "name": "Susan Khatri",
+        "birthday": "1997-07-15",
+        "nationality": "Nepali",
+        "studyProgramme": "mmd",
+        "children": "none",
+        "skills": [
+
+            {
+                "name": "js",
+                "level": "1",
+
+        },
+            {
+                "name": "css",
+                "level": "1",
+
+        },
+    ],
+        "gender": "Male",
+        "iAmThisScaredOfJS": "JS is hard but fun!",
+},
+    {
+        "name": "Martin",
+        "birthday": "1971-06-11",
+        "nationality": "",
+        "studyProgramme": "",
+        "children": [],
+        "skills": [
+            {
+                "name": "Guitar",
+                "level": 5
+		},
+            {
+                "name": "Kunst",
+                "level": 4
+		}
+
+	],
+        "gender": "male",
+        "iAmThisScaredOfJS": "3"
+},
+    {
+        "name": "Ester",
+        "birthday": "1999-02-18",
+        "nationality": "czech",
+        "studyProgramme": "MMD",
+        "children": [],
+        "skills": [
+            {
+                "name": "procrastinating",
+                "level": 100
+        },
+            {
+                "name": "sleeping",
+                "level": 100
+        }
+    ],
+        "gender": "female",
+        "iAmThisScaredOfJS": "100"
+},
+    {
+        "name": "Julia",
+        "birthday": "1999-04-02",
+        "nationality": "Polish",
+        "studyProgramme": "MMD",
+        "children": [],
+        "skills": [
+            {
+                "name": "Yoga",
+                "level": 5
+        }
+    ],
+        "gender": "F",
+
+},
+    {
+        "name": "Nadia",
+        "birthday": "1986-07-019",
+        "nationality": "Australian",
+        "studyProgramme": "Multimedia Design",
+        "children": [0],
+        "skills": [
+            {
+                "name": "Professional Peanut Butter Eater",
+                "level": 10
+    },
+            {
+                "name": "Doggo Walker",
+                "level": 10
+    }
+],
+        "gender": "Female",
+        "iAmThisscaredOfJS": ""
+},
+    {
+        "name": "Aivars",
+        "birthday": "1997-10-04",
+        "nationality": "Latvian",
+        "study_Programme": "MMD",
+        "children": [],
+        "skills": [
+            {
+                "name": "JS",
+                "level": 3
+        },
+            {
+                "name": "CSS",
+                "level": 1
+        }
+        ],
+        "gender": "male",
+        "iAmThisScaredOfJS": "super",
+},
+    {
+        "name": "Mégane",
+        "birthday": "1998-06-03",
+        "nationality": "German",
+        "studyProgramme": "MMD",
+        "children": [],
+        "skills": [
+            {
+                "name": "eating",
+                "level": 7
+    },
+            {
+                "name": "sleeping",
+                "level": 9
+    }
+  ],
+        "gender": "female",
+        "iAmThisScaredOfJS": 10
+},
+    {
+        "name": "Agne",
+        "birthday": "1995-12-08",
+        "nationality": "lithuanian",
+        "studyProgramme": "",
+        "children": [],
+        "skills": [
+            {
+                "name": "talking",
+                "level": 2
+        },
+
+            {
+                "name": "shopping",
+                "level": 5
+        }
+    ],
+
+        "gender": "female",
+        "iAmThisScaredOfJS": "6"
+},
+    {
+        "name": "Katrina",
+        "birthday": "1999-11-02",
+        "nationality": "",
+        "studyProgramme": "",
+        "children": [],
+        "skills": [{
+                "name": "cooking",
+                "level": 8,
+        },
+            {
+                "name": "drawing",
+                "level": 7,
+            }],
+        "gender": "F",
+        "IamThisAfraidOfJS": 10000,
+},
+    {
+        "name": "Sigrun",
+        "birthday": "1990-08-15",
+        "nationality": "Icelandic",
+        "studyProgramme": "MultiMedia",
+        "children": [1],
+        "skills": [{
+                "name": "illustrating",
+                "level": 3
+    },
+            {
+                "name": "sewing",
+                "level": 1
+    }
+    ],
+        "gender": "female",
+        "IAmThisScaredOfJs": [2]
+},
+    {
+        "name": "Patrick Martins",
+        "birthday": "1999-02-17",
+        "nationality": "danish, portuguese",
+        "studyProgramme": "MMD",
+        "children": [],
+        "skills": [
+            {
+                "name": "football",
+                "level": 2
+        },
+            {
+                "name": "CSS",
+                "level": 1
+        }
+        ],
+        "gender": "male",
+        "iAmThisScaredOfJS": 1
+},
+    {
+        "name": "Sakina",
+        "birthday": "1996-06-04",
+        "naionality": "Danish",
+        "studyProgram": "MMD",
+        "children": [],
+        "skills": [
+            {
+                "name": "dancing",
+                "level": "7",
+        },
+            {
+                "name": "singing",
+                "level": "6",
+        }
+    ],
+        "gender": "Female",
+        "iAmThisScaredOfJS": "Still to be determined",
+},
+    {
+
+        "name": "Jonas",
+        "birthday": "1978-08-06",
+        "nationality": "Danish",
+        "studyProgramme": "Multimedia",
+        "children": [],
+        "skills": [
+            {
+                "name": "JS",
+                "level": "high"
+	}
+
+],
+        "gender": "f",
+        "iAmThisScaredOfJS": "yes"
+},
+    {
+        "name": "Lea",
+        "birthday": "1999-04-14",
+        "nationality": "Hungarian",
+        "studyProgramme": "Multimediadesign",
+        "children": [],
+        "skills": [
+            {
+                "name": "JS",
+                "level": 1
+        },
+            {
+                "name": "CSS",
+                "level": 1
+        }
+        ],
+        "geneder": "female",
+        "iAmThisScaredOfJS": "very"
+
+},
+    {
+        "name": "Zachery",
+        "birthday": "1994-12-23",
+        "nationality": "USA",
+        "studyProgramme": "MMD",
+        "children": ["Edith"],
+        "skills": [{
+                "Name": "Drumming",
+                "Level": "7",
+        },
+            {
+                "Name": "Beer",
+                "Level": "100",
+    }],
+        "gender": "Male",
+        "IAmThisScaredOfJS": "Crying",
+
+},
+    {
+        "name": "Zoe",
+        "birthday": "1992-06-28",
+        "nationality": [{
+                "first": "danish",
+                "second": "american"
+            },
+                   ],
+        "studyProgramme": "MMD",
+        "children": [],
+        "skills": [
+            {
+                "name": "piano",
+                "level": "5"
+        },
+            {
+                "name": "wine",
+                "level": "100",
+        },
+            {
+                "name": "monopoly",
+                "level": "2",
+        },
+    ],
+        "gender": "female",
+        "iAmThisScaredOfJS": "Much",
+},
+    {
+        "name": "Ingimar",
+        "DOB": "1993-11-23",
+        "Nationality": "",
+        "StudyProgram": "",
+        "Children": [],
+        "skills": [
+            {
+                "name": "JS",
+                "level": 3
+},
+            {
+                "name": "JS",
+                "level": 1
+},
+],
+        "gender": "",
+        "IamThisScaredOfJS": "",
+},
+    {
+        "name": "Tram Anh",
+        "birthday": "1999-12-12",
+        "nationality": "vietnamese",
+        "studyprogramme": "MMD",
+        "children": [],
+        "skills": [
+            {
+                "name": "Photoshop",
+                "level": 3
+        },
+            {
+                "name": "Illustrator",
+                "level": 4
+        }
+    ],
+        "gender": "female",
+        "iamthisscaredofjs": "10",
+},
+    {
+        "name": "Christoffer",
+        "birthday": "1995-07-20",
+        "nationality": "Danish",
+        "gender": "Male",
+        "strudyProgramme": "MMD",
+        "children": [],
+        "hobbies": ["Basketball", "Baseball", "Cooking"],
+        "skills": [{
+                "name": "Javascript",
+                "level": 3
+               },
+            {
+                "name": "Cooking",
+                "level": 10
+               }
+              ],
+        "iAmThisScaredOfJS": 1,
+        "countriesIHaveBeenTo": [
+            {
+                "name": "Germany",
+                "numberOfVisits": 6,
+        },
+            {
+                "name": "United States of America",
+                "numberOfVisits": 2
+        },
+            {
+                "name": "Indonesia",
+                "numberOfVisits": 10,
+        },
+            {
+                "name": "Australia",
+                "numberOfVisits": 1
+        },
+    ]
+},
+    {
+        "name": "Indre",
+        "birthday": "1989-10-17",
+        "nationality": "lithuanian",
+        "studyPrograme": "MMD",
+        "children": [],
+        "skills": [{
+            "name": "sleeping",
+            "level": 5
+  }, {
+            "name": "drinking coffe",
+            "level": 5
+  }, {
+            "name": "CSS",
+            "level": 3
+  }],
+        "genger": "female",
+        "IamThisScaredOfJs": "3"
+},
+    {
+        "name": "Luciano",
+        "birthday": "1986-29-12",
+        "nationality": "",
+        "children": [],
+        "skills": [
+            {
+                "name": "Basketball",
+                "level": 6
+        },
+            {
+                "name": "Eat",
+                "level": 7
+        }
+    ],
+
+        "gender": "Male",
+
+        "scaredOfJS": "Yes",
+        "level": 8
+}
+];
+console.log(students);
